91a7d675fad0e84786eb60ef0ebd01639cd28b21
pornpan sitthinan <pornpan00048@gmail.com>
2022-06-25 11:28:56 +0700

    add Loading progress in register page

--- lib/pages/accounts/register.dart
+++ lib/pages/accounts/register.dart
@@ -3,6 +3,7 @@ import 'package:gsb_day03/components/user_form.dart';
 import 'package:gsb_day03/models/http_status_msg.dart';
 import 'package:gsb_day03/services/user_service.dart';
 import 'package:gsb_day03/utils/alert_helper.dart';
+import 'package:gsb_day03/utils/loading_progress.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -26,12 +27,14 @@ class _RegisterScreenState extends State<RegisterScreen> {
   }
 
   _submit(String username, String password) async{
-    UserService userService = UserService();
-    HttpStatusMsg htm = await userService.register(username, password);
-    if(htm.success){
-      AlertHelper.showBar(context: context, msg: 'complete');
-    }else{
-      AlertHelper.showBar(context: context, msg: htm.err! ,isError: true);
-    }
+    LoadingProgress.inProgress(() async{
+      UserService userService = UserService();
+      HttpStatusMsg htm = await userService.register(username, password);
+      if(htm.success){
+        AlertHelper.showBar(context: context, msg: 'complete');
+      }else{
+        AlertHelper.showBar(context: context, msg: htm.err! ,isError: true);
+      }
+    });
   }
 }
