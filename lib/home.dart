d8fbb4cbdaf110b50a3d6a22748f2d57f382a4df
pornpan sitthinan <pornpan00048@gmail.com>
2022-06-25 11:54:10 +0700

    add setting page

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gsb_day03/pages/tabs/setting.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,7 +8,14 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+final tabs = [
+  Center(child: Text('Home')),
+  SettingScreen()
+
+];
+
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +23,21 @@ class _HomeScreenState extends State<HomeScreen> {
         leading: Icon(Icons.inventory),
         title: Text('Home'),
       ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.lightGreen,
+        onTap: _onItemTapped ,
+      ),
     );
+  }
+
+  _onItemTapped(int index){
+    _currentIndex = index;
+    setState((){});
   }
 }
