1a1af6ed66e304f1cb72479cddb0a8cb410413c2
pornpan sitthinan <pornpan00048@gmail.com>
2022-06-26 10:45:39 +0700

    feat✨ :  add function add Inventory

--- lib/pages/inventories/add_inventory.dart
+++ lib/pages/inventories/add_inventory.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gsb_day03/models/http_status_msg.dart';
+import 'package:gsb_day03/services/inventory_service.dart';
+import 'package:gsb_day03/utils/alert_helper.dart';
+import 'package:gsb_day03/utils/loading_progress.dart';
 
 import '../../components/stock_form.dart';
 import '../../models/inventory.dart';
@@ -27,5 +31,15 @@ class _AddInventoryScreenState extends State<AddInventoryScreen> {
 
   _submit(Inventory inv) {
     // print('name: ${inv.description} stock:${inv.stock} price: ${inv.price}');
+    LoadingProgress.inProgress(() async{
+      InventoryService inventoryService = InventoryService();
+      HttpStatusMsg htm = await inventoryService.insert(inv);
+      if(htm.success){
+        AlertHelper.showBar(context: context, msg: 'Insert Complete');
+        Navigator.pop(context);
+      }else{
+        AlertHelper.showBar(context: context, msg: htm.err!, isError: true);
+      }
+    });
   }
 }
--- lib/services/inventory_service.dart
+++ lib/services/inventory_service.dart
@@ -27,4 +27,36 @@ class InventoryService{
     }
     return null;
   }
+
+  Future<HttpStatusMsg> insert(Inventory inventory) async{
+    Uri uri = Uri.parse('$HOST/inventory/');
+    String? token = await localStorage.getToken();
+    HttpStatusMsg htm = HttpStatusMsg();
+    if(token == null){
+      htm.success = false;
+      htm.err = 'Token is null';
+      return htm;
+    }
+    final res = await http.post(
+      uri,
+      headers: {
+        'Content-Type' : 'application/json',
+        'Authorization' : 'Bearer $token'
+      },
+      body: jsonEncode({
+        "description" : inventory.description,
+        "price": inventory.price,
+        "stock": inventory.stock
+      }
+      )
+    );
+    // HttpStatusMsg htm = HttpStatusMsg();
+    if(res.statusCode == 200){
+      htm.success = true;
+      return htm;
+    }
+    htm.success = false;
+    htm.err = 'Something went wrong';
+    return htm;
+  }
 }
